86b727768c70efec9f36335776fbec11dd86d010
Charles Howard <charlesincollege7@gmail.com>
2021-12-04 20:03:35 -0500

    Add project files.

---
+++ AdventOfCode21/Day1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode21
+{
+    internal class Day1
+    {
+
+
+        public void countIncreases()
+        {
+            string fileName = @"day1input.txt";
+
+            string text = System.IO.File.ReadAllText(fileName);
+
+            Console.WriteLine(text);
+
+            string[] lines = text.Split('\n');
+            int[] nums = new int[lines.Length];
+
+            int increaseCount = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                try { nums[i] = int.Parse(lines[i]); }
+                catch (FormatException e){ break; }
+            }
+
+
+
+            for(int i = 0; i < nums.Length - 1; i++)
+            {
+                if(nums[i] < nums[i + 1])
+                {
+                    increaseCount++;
+                }
+            }
+
+            Console.WriteLine("Increases: ");
+            Console.Write(increaseCount);
+
+        }
+        
+
+    }
+}
---
+++ AdventOfCode21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AdventOfCode21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Day1 day1 = new();
+
+            day1.countIncreases();
+
+
+        }
+    }
+}
